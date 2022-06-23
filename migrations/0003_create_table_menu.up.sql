22581a90a42bb78b88ce5d7454fded0e32b5d5a1
kcchandra <kcchandra@waresix.com>
2022-06-23 20:18:11 +0700

    refactor(migrate): adjust table naming

--- migrations/0003_create_table_menu.up.sql
+++ migrations/0003_create_table_menu.up.sql
@@ -1,4 +1,4 @@
-CREATE TABLE menu (
+CREATE TABLE menues (
     id uuid NOT NULL,
     name character varying NOT NULL,
     description character varying,
@@ -10,7 +10,7 @@ CREATE TABLE menu (
     PRIMARY KEY (id)
 );
 
-INSERT INTO menu (id, name, description, price, stock, image_url, created_at, updated_at)
+INSERT INTO menues (id, name, description, price, stock, image_url, created_at, updated_at)
 VALUES 
     ('f1740163-1bf3-478c-95ab-f5cc2277d8d7', 'Ote-Ote Porong', 'Isi Ayam dan Rumput Laut', 8000, 10, 'https://via.placeholder.com/150', NOW(), NOW()),
     ('52f0645e-6af7-42eb-a643-5da3a29e296c', 'Ote-Ote Porong', 'Isi Babi dan Rumput Laut', 10000, 10, 'https://via.placeholder.com/150', NOW(), NOW());
