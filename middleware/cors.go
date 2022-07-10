d7575ac3fe3cb57eb3106b704e04422e65b52759
kcchandra <kcchandra@waresix.com>
2022-07-10 17:33:32 +0700

    chore(headers): add cache control header

--- middleware/cors.go
+++ middleware/cors.go
@@ -8,6 +8,7 @@ func Cors() gin.HandlerFunc {
 		c.Header("Access-Control-Allow-Credentials", "true")
 		c.Header("Access-Control-Allow-Headers", "Access-Control-Allow-Origin, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, X-Authorization")
 		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+		c.Header("Cache-Control", "max-age=36000")
 
 		m := map[string]struct{}{
 			"GET": {}, "POST": {}, "PUT": {}, "DELETE": {},
